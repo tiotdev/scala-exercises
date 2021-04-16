45bbcc33a84e6a46deeda90d840acdfeb443e2de
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2021-04-16 22:21:21 +0200

    better test failure messages

--- koans/src/test/scala/KoansTest.scala
+++ koans/src/test/scala/KoansTest.scala
@@ -3,31 +3,31 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class KoansTest extends AnyFunSuite {
   test("returns the given string in reverse") {
-    assert(reverse("abcd") equals "dcba")
-    assert(reverse("Hello World") equals "dlroW olleH")
+    assert(reverse("abcd") === "dcba")
+    assert(reverse("Hello World") === "dlroW olleH")
   }
 
   test("checks if a given string is a palindrome, ignoring case and punctuation") {
-    assert(isPalindrome("Madam I'm Adam") equals true)
-    assert(isPalindrome("Not a Palindrome") equals false)
+    assert(isPalindrome("Madam I'm Adam") === true)
+    assert(isPalindrome("Not a Palindrome") === false)
   }
 
   test("returns the element at the nth position in a list") {
-    assert(nth(List("a", "w", "4", "7"), 2) equals "4")
-    assert(nth(List("Scala", "koans", "are", "fun"), 1) equals "koans")
+    assert(nth(List("a", "w", "4", "7"), 2) === "4")
+    assert(nth(List("Scala", "koans", "are", "fun"), 1) === "koans")
   }
 
   test("returns the first n numbers in the Fibonacci sequence") {
-    assert(fibonacci(3) equals List(1, 1, 2))
-    assert(fibonacci(6) equals List(1, 1, 2, 3, 5, 8))
-    assert(fibonacci(8) equals List(1, 1, 2, 3, 5, 8, 13, 21))
+    assert(fibonacci(3) === List(1, 1, 2))
+    assert(fibonacci(6) === List(1, 1, 2, 3, 5, 8))
+    assert(fibonacci(8) === List(1, 1, 2, 3, 5, 8, 13, 21))
   }
 
   test("returns only the odd numbers in a sequence") {
-    assert(onlyOdd(List(1,2,3,4,5,6)) equals List(1,3,5))
+    assert(onlyOdd(List(1,2,3,4,5,6)) === List(1,3,5))
   }
 
   test("zips two lists together to form a map") {
-    assert(zipMap(List("Hello", "World"), List("Hallo", "Welt")) equals Map("Hello" -> "Hallo", "World" -> "Welt"))
+    assert(zipMap(List("Hello", "World"), List("Hallo", "Welt")) === Map("Hello" -> "Hallo", "World" -> "Welt"))
   }
 }
